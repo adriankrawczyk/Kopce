4500b18ec182c33351d35e61bebb54164cc1551d
adriankrawczyk <adriankrawczyk0@o2.pl>
2024-11-13 12:21:55 +0100

    fix 2

--- src/home.js
+++ src/home.js
@@ -2,9 +2,7 @@ import { hills } from "./hills.js";
 import { hillText } from "./hillText.js";
 
 function home() {
-  alert(1);
   document.addEventListener("DOMContentLoaded", () => {
-    alert(2);
     hills();
     hillText();
   });
